a70ed5a6e14e7b2af1dc6f17f48cb406d3f27be5
kamelMohsen <kamelmohsenkamel@gmail.com>
2020-03-27 10:11:23 +0200

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Kamel Mohsen on 27/03/2020.
+//  Copyright © 2020 Kamel Mohsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
